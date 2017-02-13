14337181a9ede224106bda87e056ab243f4ec546
lions2907 <frclionrobotics@gmail.com>
2017-02-12 17:43:43 -0800

    Update RotateToAngle.java

--- src/org/usfirst/frc/team2907/robot/commands/RotateToAngle.java
+++ src/org/usfirst/frc/team2907/robot/commands/RotateToAngle.java
@@ -49,7 +49,7 @@ public class RotateToAngle extends Command {
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return false;
+		return pidController.onTarget();
 	}
 
 	// Called once after isFinished returns true
