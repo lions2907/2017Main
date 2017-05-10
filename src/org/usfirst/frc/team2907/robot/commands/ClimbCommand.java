5971d581c810d2bc9104308a3deed69a43385912
lions2907 <frclionrobotics@gmail.com>
2017-05-09 17:43:41 -0700

    Final

--- src/org/usfirst/frc/team2907/robot/commands/ClimbCommand.java
+++ src/org/usfirst/frc/team2907/robot/commands/ClimbCommand.java
@@ -6,9 +6,7 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class ClimbCommand extends Command
 {
-	/* THIS COMMAND CONTROLS THE CLIMBER VIA ANALOG JOYSTICK */
 	public static final int JOYSTICK_AXIS = 1; // LEFT AXIS UP-DOWN
-	public static final double POWER_MAX = .75; // BEST TORQUE AT 75% POWER?
 	
 	public ClimbCommand()
 	{
@@ -18,19 +16,19 @@ public class ClimbCommand extends Command
 	
 	protected void execute()
 	{
+		/* if guitra is not enabled use ps4 */
 		if (!Robot.oi.isGuitarMode())
 		{
-			if (Robot.oi.manipulatorStick.getY() < 0) // ONLY DRIVE ONE WAY CAUSE LOCKING MECHANISM
+			if (Robot.oi.manipulatorStick.getY() < 0) // Only drive on way because of locking mechanism
 			{
 				Robot.climber.climb(Robot.oi.manipulatorStick.getRawAxis(JOYSTICK_AXIS));
-				//Robot.climber.climb(-(Math.min(Robot.oi.manipulatorStick.getRawAxis(JOYSTICK_AXIS), POWER_MAX)));
 			} else 
 			{
 				Robot.climber.climb(0);
 			}
 		} else 
 		{
-			
+			/* guitar not implemented yet */
 		}
 	}
 
